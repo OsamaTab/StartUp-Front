8b13c4cbc8e8db9c07eabbab097de2d00a849e7f
OsamaTap <darkiisouls@outlook.com>
2020-01-14 01:20:17 +0200

    adding bootstrab

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,28 @@ import Login from './login';
 
 class App extends React.Component {
 	constructor() {
-		super();
-	}
-	componentDidMount() {
+        super()
+        this.state = {
+            isLoading: true
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 500)
+    }
 
-	}
-	
 	render() {
+		if(this.state.isLoading == true){
+            return (
+                <div className='d-flex w-100' style={{height:'100vh'}}>
+                    <div className="lds-dual-ring mx-auto my-auto" ></div>
+                </div>
+              )
+		}
+		
 		return (
 			<Login/>
 		);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+import './bootstrap.css'
 import './index.css';
+
 import App from './components/App';
 
 
