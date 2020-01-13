644fc43d5587c950f2cfdd059710c8c39fb6155c
OsamaTap <darkiisouls@outlook.com>
2020-01-14 01:01:45 +0200

    login page is done

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Login from './login';
+
+class App extends React.Component {
+	constructor() {
+		super();
+	}
+	componentDidMount() {
+
+	}
+	
+	render() {
+		return (
+			<Login/>
+		);
+	}
+
+}
+
+export default App;
